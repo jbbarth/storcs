8856e21a17290ca709535633aac4ad6fab96e83f
jbbarth <jeanbaptiste.barth@gmail.com>
2011-01-04 16:55:23 +0100

    Re-ordered Ibm Parser specs

--- spec/parsers/ibm_spec.rb
+++ spec/parsers/ibm_spec.rb
@@ -1,6 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 describe Storcs::Parsers::Ibm do
+  it "parses the output of a 'SMcli X.X.X.X 'show storagesubsystem profile;'" do
+    parsed = Storcs::Parsers::Ibm.new('ds4500','spec/data/ibm_DS4500.txt')
+    bay = parsed.device
+    bay.should_not be_nil
+    bay.name.should == "ds4500"
+    arrays = bay.children
+    arrays.length.should == 15
+    bay.size.should == 12285713148014
+    bay.used.should == 11460324586680
+    bay.free.should == 825388561334
+  end
+
   it "divides a profile file into small sections" do
     sections = Storcs::Parsers::Ibm.new('ds4500','spec/data/ibm_DS4500.txt').sections
     sections.should be_a Hash
@@ -28,17 +40,5 @@ describe Storcs::Parsers::Ibm do
     first.used.should == first.size
     array.size.should == 1316496596795
     array.used.should == 655826473713
-  end
-
-  it "parses the output of a 'SMcli X.X.X.X 'show storagesubsystem profile;'" do
-    parsed = Storcs::Parsers::Ibm.new('ds4500','spec/data/ibm_DS4500.txt')
-    bay = parsed.device
-    bay.should_not be_nil
-    bay.name.should == "ds4500"
-    arrays = bay.children
-    arrays.length.should == 15
-    bay.size.should == 12285713148014
-    bay.used.should == 11460324586680
-    bay.free.should == 825388561334
   end
 end
