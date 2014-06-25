b794b7cc35a45f5b4cc879c28237061d41467403
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-06-25 04:12:09 +0200

    Explicitly enable the old 'should' syntax in rspec 3.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,11 @@ $:.unshift File.expand_path("../../lib", __FILE__)
 require 'storcs'
 require 'rspec'
 require 'rspec/collection_matchers'
+
+#explicitly enable "should" syntax in rspec 3.x
+#see: https://www.relishapp.com/rspec/rspec-expectations/docs/syntax-configuration#disable-should-syntax
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
